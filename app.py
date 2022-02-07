9ce543a36a0b6aad1a919b23e177e6a1eb31904b
shujaakbar2020 <shujaakbar2020@gmail.com>
2022-02-07 16:48:10 +0500

    Latest position for moving ball

--- app.py
+++ app.py
@@ -1,6 +1,17 @@
 from kivy.app import App
 from kivy.uix.widget import Widget
+from kivy.properties import NumericProperty, RefrenceListProperty
+from kivy.vector import Vector
 
+
+class PongBall(Widget):
+    velocity_x = NumericProperty(0)
+    velocity_y = NumericProperty(0)
+    velocity = RefrenceListProperty(velocity_x, velocity_y)
+
+    # Latest Position = Current Velocity + Current Position
+    def move(self):
+        self.pos = Vector(*self.velocity) + self.pos
 
 class PongGame(Widget):
     pass
