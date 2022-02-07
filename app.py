a7fff8b26826badddd84ec5d0d716a810f87b8aa
shujaakbar2020 <shujaakbar2020@gmail.com>
2022-02-07 16:27:23 +0500

    Getting started

---
+++ app.py
@@ -0,0 +1,14 @@
+from kivy.app import App
+from kivy.uix.widget import Widget
+
+
+class PongGame(Widget):
+    pass
+
+
+class PongApp(App):
+    def build(self):
+        return PongGame()
+
+
+PongApp().run()
